677d07081bee9c7576fd28659dcf09af22ddb2b2
Stefan Coe <coestefan@gmail.com>
2012-01-24 18:57:46 -0800

    update echo

--- echo/recho_client.py
+++ echo/recho_client.py
@@ -1,18 +1,20 @@
-
 """
-echo client, usage:
+recho client, usage:
 
- python echo_client.py <host> <port>
+ python recho_client.py <host> <port>
 
 Both host and port are optional, defaults: localhost 50000
 host must be present if you want to provide port
+
+Prompt user for each message to send
+Repeat sending messages until user enters empty string
 """
 
 import socket 
 import sys
 
 host = 'localhost' 
-port = 50000 
+port = 50002 # different default port than echo, both can run on same server
 size = 1024 
 
 nargs = len(sys.argv)
@@ -21,17 +23,18 @@ if nargs > 1:
 if nargs > 2:
     port = int(sys.argv[2])
 
-
-i = "hold" 
-while not len(i) == 0:
-    s = socket.socket(socket.AF_INET, 
+s = socket.socket(socket.AF_INET, 
                   socket.SOCK_STREAM) 
-    s.connect((host,port))
+s.connect((host,port)) 
+print 'Connection accepted by (%s,%s)' % (host, port)
+while True:
+    msg = raw_input('> ')
+    if msg:         # msg is not empty
+        s.send(msg) 
+        data = s.recv(size)
+        print data
+    else:          # msg is empty
+        s.close() 
+        break      # exit loop            
 
-    i = input("please type something")
-    s.send(i) 
-    data = s.recv(size) 
- 
-    print 'from (%s,%s) %s' % (host, port, data)
 
-    s.close()
