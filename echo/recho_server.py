88cd740a9f33c33eaeb5dbd41ad6189948c8dd24
Stefan Coe <coestefan@gmail.com>
2012-01-31 16:46:26 -0800

    add echo....

--- echo/recho_server.py
+++ echo/recho_server.py
@@ -1,47 +1,41 @@
 """
-recho server, usage:
+recho client, usage:
 
- python recho_server.py <port>
+ python recho_client.py <host> <port>
 
-Port is optional, default: 50002
+Both host and port are optional, defaults: localhost 50000
+host must be present if you want to provide port
+
+Prompt user for each message to send
+Repeat sending messages until user enters empty string
 """
 
 import socket 
 import sys
 
-host = '' 
-port = 50002 # different default port than echo, both can run on same server
+host = 'localhost' 
+port = 50003 # different default port than echo, both can run on same server
+size = 1024 
 
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
 
-backlog = 5 
-#size = 1024
-size = 10
+s = socket.socket(socket.AF_INET, 
+                  socket.SOCK_STREAM) 
+s.connect((host,port)) 
+print 'Connection accepted by (%s,%s)' % (host, port)
+while True:
+    msg = raw_input('> ')
+    if msg:         # msg is not empty
+        s.send(msg) 
+        data = s.recv(size)
+        print data
+    else:          # msg is empty
+        s.close() 
+        break      # exit loop            
 
-# server's listener socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-# Release listener socket immediately when program exits, 
-# avoid socket.error: [Errno 48] Address already in use
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-s.bind((host,port)) 
-
-print 'recho_server listening on port', port
-s.listen(backlog) 
-
-while True: 
-    client, address = s.accept()
-    print 'accepted connection from ', address
-    while True:
-        data = client.recv(size) 
-        if data:
-            client.send('uw-student: %s' % data) 
-        else:  # no data, client sent empty message or closed socket
-            client.close()
-            print 'closed connection'
-            break
-
-        
 
