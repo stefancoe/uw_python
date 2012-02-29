3a52c6763773cf1481688fbd22f2bb5e2d0b4d1d
Stefan Coe <coestefan@gmail.com>
2012-02-28 16:35:46 -0800

    add week7 homework

--- week7_homework/books_flask_.py
+++ week7_homework/books_flask_.py
@@ -7,35 +7,29 @@ To get started, point browser at echo_flask.html
 
 from flask import Flask, request
 import bookdb
-
 # no need for template here - just a constant string
-titles_template = """<head>
-
+form_page = """<head>
 <title>Echo request</title>
 </head>
 <body>
 <form method="GET" action="echo_flask.py">
-Message %s
+Message: <input type="text" name="message" size="40">
 <input type="submit" value="Submit">
 </form>
 </body>
 </html>
 """
-titles_template2 = """
+
+message_template2 = """
 <html>
 <head>
 <title>Echo response</title>
 </head>
 <body>
-Message %s
+%s
 </body>
 </html>
 """
-
-
-
-# Global varible
-
 
 # No need for message page
 # Flask converts view function return string to HTML page
@@ -46,30 +40,44 @@ app.debug = True # development only - remove on production machines
 
 # View functions generate HTTP responses including HTML pages and headers
 
-@app.route('/test.html')
+@app.route('/echo_flask.html')
 def form():
     books = bookdb.BookDB()
-    titles = str(books.titles())
-    #assert len(titles) > 1
-    #print titles
+    titles = books.titles()
+        #assert len(titles) > 1
+        #print titles
     messages = ""
     for title in titles:
-        x = str(title["title"])
-
-        messages = ('%s<br>\n' % x) + messages # insert at head
-    page = titles_template % messages
-
-    
-    return  page  # list of strings - must return iterable, not just a string
-
-
-    #return form_page
-
+        x = title["title"]
+        link = '<a href= ' + '"/' + x + '"' + '>%s</a><br>'
+        messages = (link %x) + messages
+        #messages = ('<a href=%s>%s</a><br>' %x) + messages # insert at head
+        
+        page = message_template2 % messages
+    return  page  # li
 @app.route('/echo_flask.py')
 def message_page():
     # Flask Quickstart suggests request.form should work, but here it is empty
     # Flask converts return string to HTML page
     return 'Message: %s' % request.args['message']
+
+@app.route('/<bookTitle>')
+def book_info(bookTitle):
+    books = bookdb.BookDB()
+    titles = books.titles()
+    messages = ""
+    for title in titles:
+        x = title["title"]
+        if x == bookTitle:
+            y = title["id"]
+            
+            messages = books.title_info(y)
+            page = message_template2 % messages
+        else:
+            message = "not found"
+            page = message_template2 % messages
+    return page 
+     
 
 # No function needed for other routes - Flask will send 404 page
 
